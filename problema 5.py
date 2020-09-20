6023bc439db7af3a925f1bcb683016bb3550e89e
Cebotari Anastasia <71016019+anastasiacebotari15@users.noreply.github.com>
2020-09-20 14:14:58 +0300

    Add files via upload

---
+++ problema 5.py
@@ -0,0 +1,14 @@
+x= True
+y=False
+print(x,"&", y, "=", x & y)
+print(y, "&", x, "=", y & x)
+print(x, "&", x, "=", x & x)
+print(y, "&", y, "=", y & y )
+
+print(x,"or", y, "=", x or y)
+print(y, "or", x, "=", y or x)
+print(x, "or", x, "=", x or  x)
+print(y, "or", y, "=", y or y )
+
+print(x, "not", "=", not x)
+print(y, "not", "=", not y)
---
+++ problema3.py
@@ -0,0 +1,4 @@
+a= 10
+print( a*4)
+print(a**2*6)
+print(  a**3)
